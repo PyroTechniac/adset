337432a8d90a13fcd549ef3b5ccfe47d91cccbcb
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-02-20 10:29:28 -0500

    added more searching and jsdoc comments

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ class Betterset extends Set {
     }
     /**
      * Returns a betterset of all the arrays in the set
+     * @returns {Betterset}
      */
     arrays() {
         const results = new this.constructor();
@@ -18,10 +19,25 @@ class Betterset extends Set {
         }
         return results;
     }
+    /**
+     * Returns a betterset of all the strings in the set
+     * @returns {Betterset}
+     */
     strings() {
         const results = new this.constructor();
         for (const val of this) {
             if (val instanceof String) results.add(val);
+        }
+        return results;
+    }
+    /**
+     * Returns a betterset of all the objects in the set
+     * @returns {Betterset}
+     */
+    objects() {
+        const results = new this.constructor();
+        for (const val of this) {
+            if (val instanceof Object && val.constructor === Object) results.add(val);
         }
         return results;
     }
@@ -36,8 +52,22 @@ class Betterset extends Set {
         return set;
     }
     /**
+     * Returns a betterset of all the numbers in the set
+     * @returns {Betterset}
+     */
+    numbers() {
+        const results = new this.constructor();
+        for (const val of this) {
+            if (typeof val === 'number') results.add(val);
+        }
+        return results;
+    }
+    /**
      * Identical to [`Set.forEach()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/forEach) but
      * returns the betterset instead of undefined
+     * @param {Function} fn The function to run
+     * @param {*} thisArg The argument to use as `this`
+     * @returns {Betterset}
      */
     each(fn, thisArg) {
         this.forEach(fn, thisArg);
--- test/main.js
+++ test/main.js
@@ -3,4 +3,4 @@ const set = new Betterset();
 set.add(['Test 1', 2]);
 set.add(3);
 set.add({ name: 'Hello world' });
-console.log(set.strings());
+console.log(set.numbers());
